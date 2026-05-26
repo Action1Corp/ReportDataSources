51c6c0e3efded96e03a48402eff2aa5c7be0d300
Aleksy Rembicki <Aleksy.Rembicki.c@action1.com>
2026-05-26 11:19:32 +0200

    Add legal notices. Add legal header. (#2)
    Add legal notices
    Add legal headers
    Update readme.md

--- DellWarrantyCheck.ps1
+++ DellWarrantyCheck.ps1
@@ -1,21 +1,17 @@
 ﻿# Name: DellWarrantyCheck.ps1
 # Description: Will check against Dell's API and present Warranty information on your Dell devices
-# Copyright (C) 2024 Action1 Corporation
+
 # Documentation: https://www.action1.com/documentation/data-sources/
 # Use Action1 Roadmap system (https://roadmap.action1.com/) to submit feedback or enhancement requests.
 
 # WARNING: Carefully study the provided scripts and components before using them. Test in your non-production lab first.
 
-# LIMITATION OF LIABILITY. IN NO EVENT SHALL ACTION1 OR ITS SUPPLIERS, OR THEIR RESPECTIVE 
-# OFFICERS, DIRECTORS, EMPLOYEES, OR AGENTS BE LIABLE WITH RESPECT TO THE WEBSITE OR
-# THE COMPONENTS OR THE SERVICES UNDER ANY CONTRACT, NEGLIGENCE, TORT, STRICT 
-# LIABILITY OR OTHER LEGAL OR EQUITABLE THEORY (I)FOR ANY AMOUNT IN THE AGGREGATE IN
-# EXCESS OF THE GREATER OF FEES PAID BY YOU THEREFOR OR $100; (II) FOR ANY INDIRECT,
-# INCIDENTAL, PUNITIVE, OR CONSEQUENTIAL DAMAGES OF ANY KIND WHATSOEVER; (III) FOR
-# DATA LOSS OR COST OF PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; OR (IV) FOR ANY
-# MATTER BEYOND ACTION1’S REASONABLE CONTROL. SOME STATES DO NOT ALLOW THE
-# EXCLUSION OR LIMITATION OF INCIDENTAL OR CONSEQUENTIAL DAMAGES, SO THE ABOVE
-# LIMITATIONS AND EXCLUSIONS MAY NOT APPLY TO YOU.
+# Action1 Public Repository Material
+# Subject to TERMS_OF_USE.md (https://github.com/Action1Corp/PSAction1/blob/main/TERMS_OF_USE.md)
+# Provided AS IS
+# Use at your own risk
+# Review and test before production deployment
+# © Action1 Corporation
 
 # MAKE SURE TO INSERT DELL API AND SECRET BELOW
 
